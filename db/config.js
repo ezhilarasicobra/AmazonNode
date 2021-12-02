d792733945895f51a8079e1e6a26eb987f976249
Ezhilarasi <tezzhilarasi@gmail.com>
2021-12-02 12:23:03 +0530

    Added mongo cluster

--- db/config.js
+++ db/config.js
@@ -1,6 +1,6 @@
 const mongoose =require('mongoose')
 
-mongoose.connect('mongodb+srv://ezhil:ezhil@cluster0.pb8jx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+mongoose.connect('mongodb+srv://ezhil:ezhil@cluster0.t371r.mongodb.net/AmazonClone?retryWrites=true&w=majority',{
     useCreateIndex:true,
     useNewUrlParser:true,
     useUnifiedTopology:true,
